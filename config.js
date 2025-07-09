c468347e950c5bc145f97130d334802f696490ec
ABUZAR-alt <hashimbhai998@gmail.com>
2025-07-10 02:59:45 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config/config.js
+
+const config = {
+  botName: 'ABUZAR-XD',
+  ownerNumber: ['923000000000'], // Apna number yahan likho
+  sessionName: 'session',
+  prefix: ['!', '.', '/'], // Commands ka prefix
+  author: 'Abuzar',
+  packname: 'ABUZAR-XD Pack',
+};
+
+module.exports = config;
